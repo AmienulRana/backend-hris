93c80e1295076f9bf09c7c4da55c49fc7939d49c
AmienulRana <amienulrana@gmail.com>
2023-01-02 14:09:55 +0700

    menambahkan controller untuk detail departement, dan edit departement

--- departement/controller.js
+++ departement/controller.js
@@ -38,6 +38,39 @@ module.exports = {
       res.status(500).json({ message: "Failed to Add new departement" });
     }
   },
+  editDepartement: async (req, res) => {
+    try {
+      const { dep_name, dep_manager, dep_desc, dep_location, dep_workshift } =
+        req.body;
+      const { id } = req.params;
+      const { role } = req?.admin;
+      console.log(role);
+      if (role === "Super Admin" || role === "App Admin") {
+        const departement = await Departement.updateOne(
+          { _id: id },
+          {
+            $set: {
+              dep_name,
+              dep_manager,
+              dep_desc,
+              dep_location,
+              dep_workshift,
+            },
+          }
+        );
+        return res
+          .status(200)
+          .json({ message: "Successfully edited Departement" });
+      } else {
+        return res
+          .status(422)
+          .json({ message: "You can't change departments" });
+      }
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ message: "Failed to edited departement" });
+    }
+  },
   getDepartement: async (req, res) => {
     try {
       const company_id = req?.admin?.company_id;
@@ -55,6 +88,18 @@ module.exports = {
         });
         res.status(200).json(departemen);
       }
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ message: "Failed to Get Departement" });
+    }
+  },
+  detailDepartement: async (req, res) => {
+    try {
+      const { id } = req?.params;
+      const departemen = await Departement.findOne({
+        _id: id,
+      });
+      res.status(200).json(departemen);
     } catch (error) {
       console.log(error);
       res.status(500).json({ message: "Failed to Get Departement" });
