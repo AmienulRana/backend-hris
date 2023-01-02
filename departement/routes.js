1bd2b14c8c82079970328a4439db7707c2746fec
AmienulRana <amienulrana@gmail.com>
2023-01-02 14:09:21 +0700

    menambahkan router untuk menghandle detail departement dan edit departement

--- departement/routes.js
+++ departement/routes.js
@@ -1,9 +1,16 @@
 const express = require("express");
 const { authenticationToken } = require("../middleware/authentication");
-const { addNewDepartement, getDepartement } = require("./controller");
+const {
+  addNewDepartement,
+  getDepartement,
+  editDepartement,
+  detailDepartement,
+} = require("./controller");
 const router = express.Router();
 
 router.post("/", authenticationToken, addNewDepartement);
+router.put("/:id", authenticationToken, editDepartement);
+router.get("/:id", detailDepartement);
 router.get("/", authenticationToken, getDepartement);
 // router.post("/login", loginCompany);
 module.exports = router;
