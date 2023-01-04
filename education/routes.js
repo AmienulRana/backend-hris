766b7d30a29d708b6d83f698d783acb402dce34e
AmienulRana <amienulrana@gmail.com>
2023-01-04 09:50:51 +0700

    menambahkan routes delete education

--- education/routes.js
+++ education/routes.js
@@ -3,12 +3,14 @@ const { authenticationToken } = require("../middleware/authentication");
 const {
   addNewEducation,
   getEducation,
+  deleteEducation,
   editEducation,
 } = require("./controller");
 const router = express.Router();
 
 router.post("/", authenticationToken, addNewEducation);
 router.put("/:id", authenticationToken, editEducation);
+router.delete("/:id", authenticationToken, deleteEducation);
 // router.get("/:id", detailDepartement);
 router.get("/", authenticationToken, getEducation);
 // router.post("/login", loginCompany);
