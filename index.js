1367ac77ed81418805145c91d6a4e7bd3e329933
AmienulRana <amienulrana@gmail.com>
2023-01-05 03:32:36 +0700

    membuat routes, controller, model untuk bank

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ const designationRoute = require("./designations/routes");
 const employmentRoute = require("./employee/routes");
 const EducationRoute = require("./education/routes");
 const ExperienceRoute = require("./experience/routes");
+const BankRoute = require("./bank/routes");
 const path = require("path");
 const router = express.Router();
 
@@ -31,6 +32,7 @@ app.use(`/${api_version}/designation`, designationRoute);
 app.use(`/${api_version}/employment`, employmentRoute);
 app.use(`/${api_version}/education`, EducationRoute);
 app.use(`/${api_version}/experience`, ExperienceRoute);
+app.use(`/${api_version}/bank`, BankRoute);
 
 app.listen(process.env.PORT, () => {
   console.log(`Server start running on port ${process.env.PORT}`);
