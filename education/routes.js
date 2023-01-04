4dee8f2a2fea871157b11883ab0f5c4e4143f4c6
AmienulRana <amienulrana@gmail.com>
2023-01-04 09:32:12 +0700

    menmabahkan route yang menhandle endpoint put

--- education/routes.js
+++ education/routes.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const { authenticationToken } = require("../middleware/authentication");
-const { addNewEducation } = require("./controller");
+const {
+  addNewEducation,
+  getEducation,
+  editEducation,
+} = require("./controller");
 const router = express.Router();
 
 router.post("/", authenticationToken, addNewEducation);
-// router.put("/:id", authenticationToken, editDepartement);
+router.put("/:id", authenticationToken, editEducation);
 // router.get("/:id", detailDepartement);
-// router.get("/", authenticationToken, getDepartement);
+router.get("/", authenticationToken, getEducation);
 // router.post("/login", loginCompany);
 module.exports = router;
