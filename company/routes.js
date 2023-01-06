75b530e02903ce33cb14f20cfa4db6f5903168c5
AmienulRana <amienulrana@gmail.com>
2023-01-07 05:48:59 +0700

    menambahka  routes untuk dashboard

--- company/routes.js
+++ company/routes.js
@@ -4,10 +4,12 @@ const {
   registerCompany,
   loginCompany,
   getAllCompany,
+  dahsboard,
 } = require("./controller");
 const router = express.Router();
 
 router.post("/registrasi", registerCompany);
 router.post("/login", loginCompany);
 router.get("/all", getAllCompany);
+router.get("/dashboard", dahsboard);
 module.exports = router;
