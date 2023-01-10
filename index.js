b433210378b6582cefdc2977ad7e3c1996953d45
AmienulRana <amienulrana@gmail.com>
2023-01-10 13:32:13 +0700

    menambahkan header method

--- index.js
+++ index.js
@@ -19,11 +19,12 @@ const path = require("path");
 const router = express.Router();
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "https://avapps.vercel.app");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
   );
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   next();
 });
 // app.use(cors());
