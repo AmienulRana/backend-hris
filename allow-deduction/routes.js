89eb6b376f0b8f8d0160ac96271453506e29212a
AmienulRana <amienulrana@gmail.com>
2023-01-10 14:22:15 +0700

    menambahkan routes, model, controlle untuk menghandle collections allow dedcution

---
+++ allow-deduction/routes.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const { authenticationToken } = require("../middleware/authentication");
+const {
+  addAllowDeduct,
+  getAllowDeduct,
+  editAllowDeduct,
+  changeStatusAllowDeduct,
+} = require("./controller");
+const router = express.Router();
+
+router.post("/", authenticationToken, addAllowDeduct);
+router.put("/:id", authenticationToken, editAllowDeduct);
+router.get("/:id", authenticationToken, changeStatusAllowDeduct);
+router.get("/", authenticationToken, getAllowDeduct);
+// router.post("/login", loginCompany);
+module.exports = router;
