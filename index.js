43f96aee7725ebeb6d406619bfa8070b9f9e3168
AmienulRana <amienulrana@gmail.com>
2023-01-10 13:45:59 +0700

    memberikan semua akses origin

--- index.js
+++ index.js
@@ -19,20 +19,7 @@ const path = require("path");
 const router = express.Router();
 
 app.use((req, res, next) => {
-  if (req.method === "OPTIONS") {
-    res.header("Access-Control-Allow-Origin", "https://avapps.vercel.app");
-    res.header(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-    );
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-    next();
-  }
-  next();
-});
-
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://avapps.vercel.app");
+  res.header("Access-Control-Allow-Origin", "*");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
