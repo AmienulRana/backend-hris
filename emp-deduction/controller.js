a6727d2b3bfc590ca41c6e571145df2a5d98c03e
AmienulRana <amienulrana@gmail.com>
2023-01-10 14:22:38 +0700

    menambahkan routes, model, controlle untuk menghandle collections deduction employment

---
+++ emp-deduction/controller.js
@@ -0,0 +1,139 @@
+const Deduction = require("./model");
+const Employment = require("../employee/model");
+const AllowDeduct = require("../allow-deduction/model");
+
+module.exports = {
+  addDeduction: async (req, res) => {
+    try {
+      const {
+        empallow_allowance_id,
+        empallow_allowance_amount,
+        empallow_allowance_status,
+        emp_id,
+        empallow_allowance_type,
+      } = req.body;
+      const { role } = req.admin;
+      if (role === "Super Admin" || role === "App Admin") {
+        const deduction = new Deduction({
+          emp_id,
+          empallow_allowance_id,
+          empallow_allowance_amount,
+          empallow_allowance_status,
+          empallow_allowance_type,
+        });
+        await deduction.save();
+        return res
+          .status(200)
+          .json({ message: `Successfully added deduction` });
+      }
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ message: `Failed to Add new deduction` });
+    }
+  },
+  editDeduction: async (req, res) => {
+    try {
+      const {
+        empallow_allowance_id,
+        empallow_allowance_amount,
+        empallow_allowance_type,
+      } = req.body;
+      const { role } = req.admin;
+      const { id } = req.params;
+      if (role === "Super Admin" || role === "App Admin") {
+        const deduction = await Deduction.updateOne(
+          { _id: id },
+          {
+            $set: {
+              empallow_allowance_id,
+              empallow_allowance_amount,
+              empallow_allowance_type,
+            },
+          }
+        );
+        if (deduction.modifiedCount > 0) {
+          return res
+            .status(200)
+            .json({ message: `Successfully updated deduction` });
+        }
+        return res.status(422).json({ message: `No data changed` });
+      }
+    } catch (error) {
+      res.status(500).json({ message: `Failed to edit deduction` });
+    }
+  },
+  getDeductOptionsEmp: async (req, res) => {
+    try {
+      const emp_id = req?.params?.id;
+      const employment = await Employment.findOne({ _id: emp_id });
+      const findAllDeduction = await AllowDeduct.find({
+        company_id: employment?.company_id,
+        ad_type: "Deduction",
+        ad_status: true,
+      });
+      res.status(200).json(findAllDeduction);
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ message: "Internal Server Error" });
+    }
+  },
+  getDeduction: async (req, res) => {
+    try {
+      const emp_id = req?.params?.id;
+      const deduction = await Deduction.find({
+        emp_id,
+      }).populate({
+        path: "empdeduct_deduction_id",
+        select: "ad_name _id",
+      });
+      res.status(200).json(deduction);
+    } catch (error) {
+      res.status(500).json({ message: "Internal Server Error" });
+    }
+  },
+  deleteDeduction: async (req, res) => {
+    try {
+      const { role } = req.admin;
+      const { id } = req.params;
+      if (role === "Super Admin" || role === "App Admin") {
+        const deduction = await Deduction.deleteOne({ _id: id });
+        if (deduction.deletedCount > 0) {
+          return res
+            .status(200)
+            .json({ message: "Successfully deleted Deduction" });
+        }
+      } else {
+        return res.status(422).json({ message: "Failed To Delete" });
+      }
+    } catch (error) {
+      res.status(500).json({ message: "Failed to deleted | Server Error" });
+    }
+  },
+  changeStatusDeduction: async (req, res) => {
+    try {
+      const { role } = req.admin;
+      const { id } = req.params;
+      if (role === "Super Admin" || role === "App Admin") {
+        const findDeduction = await Deduction.findOne({ _id: id });
+        const deduction = await Deduction.updateOne(
+          { _id: id },
+          {
+            $set: {
+              empallow_deduction_status: findDeduction.empallow_deduction_status
+                ? false
+                : true,
+            },
+          }
+        );
+        return res.status(200).json({
+          message: `Successfully updated status`,
+        });
+      }
+    } catch (error) {
+      console.log(error);
+      res
+        .status(500)
+        .json({ message: `Failed to edit status | Internal Server Error` });
+    }
+  },
+};
